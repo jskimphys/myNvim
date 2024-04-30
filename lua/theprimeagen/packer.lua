b79117507017c3989defaff60a4ae4d0157f8d7d
jskim <jskimphys@gmail.com>
2024-04-30 19:27:28 +0900

    todo message plugin

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -94,6 +94,15 @@ return require('packer').startup(function(use)
 
   use('sakshamgupta05/vim-todo-highlight')
 
+  -- todo comments
+  use{
+    'folke/todo-comments.nvim',
+    requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require('todo-comments').setup{}
+    end
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
