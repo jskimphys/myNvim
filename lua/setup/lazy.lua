eae165269588d50d808c9083f93ec9058d0beee8
jskim <jskimphys@gmail.com>
2024-05-22 11:35:35 +0900

    fi

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -78,6 +78,17 @@ plugins = {
   
   -- fzf command line completion
   "gelguy/wilder.nvim",
+
+  -- aerial => A code outline window for skimming and quick navigation
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    },
+  }
 }
 
 
