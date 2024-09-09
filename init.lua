a08ca27b6dcb6feca283174758fcfed4086d08dc
jaisung kim <jaisung.kim@snu.ac.kr>
2024-09-09 11:41:46 +0900

    clangd-format

--- init.lua
+++ init.lua
@@ -100,14 +100,25 @@ require('setup/plugins')
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+lsp.format_on_save(
+  {
+    format_opts = {
+      async = true,
+      timeout_ms = 5000,
+    },
+    servers = {
+      ['rust-analyzer'] = {'rust'},
+      ['clangd'] = {'c', 'cpp', 'cu'}
+    }
+  })
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete()
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-Space>'] = cmp.mapping.complete()
 })
 
 lsp.on_attach(function(client, bufnr)
